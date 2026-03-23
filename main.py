2b8d1ee83fe9c37f436a3369420ebf7b59ef7faf
mudamudi <mudamudiberkelas@gmail.com>
2026-03-23 19:09:48 +0700

    config

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/")
+def root():
+    return {"status": "OK", "service": "kong-gateway-app"}
+
+
 @app.get("/healthy")
 def health_check():
     return {'status': 'Healthy'}
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="localhost", port=5000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
